08062731d8da51a19b95a2ca4af42bdadae2de4c
RoYaLBG <royal4o@gmail.com>
2015-04-12 17:30:09 +0300

    #10 - Decorating product resultset with promoted price and discount

---
+++ TechShop/TechShop.WebApi/Providers/ItemDecorator.cs
@@ -0,0 +1,26 @@
+﻿namespace TechShop.WebApi.Providers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    using TechShop.Models;
+    using TechShop.Data.Data;
+    using WebApi;
+
+    public abstract class ItemDecorator<T>
+    {
+        public IEnumerable<T> ResultSet { get; private set; }
+
+        public ITechShopData Data { get; private set; }
+
+        public ItemDecorator(IEnumerable<T> resultSet, ITechShopData data)
+        {
+            this.ResultSet = resultSet;
+            this.Data = data;
+        }
+
+        public abstract void Decorate();
+    }
+}
---
+++ TechShop/TechShop.WebApi/Providers/ItemPromotionProvider.cs
@@ -0,0 +1,29 @@
+﻿namespace TechShop.WebApi.Providers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
+    using TechShop.Models;
+    using TechShop.Data.Data;
+    using WebApi;
+
+    public class ItemPromotionProvider : ItemDecorator<Product>
+    {
+        public ItemPromotionProvider(IEnumerable<Product> resultSet, ITechShopData data) 
+            : base(resultSet, data)
+        {
+        }
+
+        public override void Decorate()
+        {
+
+        }
+
+        private IEnumerable<GlobalPromotion> GetActiveGlobalPromotions()
+        {
+           return this.Data.Categories.All().Wh
+        }
+    }
+}
