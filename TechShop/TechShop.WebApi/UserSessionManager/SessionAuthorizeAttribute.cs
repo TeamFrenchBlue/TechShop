554a02ae674d0d4d1d5adfe6eea6dc7a69549487
GBurlakova <gabriela_burlakova@abv.bg>
2015-04-14 11:41:10 +0300

    Added missing file for Session authorize attribute

---
+++ TechShop/TechShop.WebApi/UserSessionManager/SessionAuthorizeAttribute.cs
@@ -0,0 +1,50 @@
+﻿namespace TechShop.WebApi.UserSessionManager
+{
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+    using System.Web.Http.Controllers;
+
+    using Data.Context;
+    using Data.Data;
+
+    public class SessionAuthorizeAttribute : AuthorizeAttribute
+    {
+        protected ITechShopData Data { get; private set; }
+
+        public SessionAuthorizeAttribute(ITechShopData data)
+        {
+            this.Data = data;
+        }
+
+        public SessionAuthorizeAttribute() : this(new TechShopData(new TechShopDbContext()))
+        {
+        }
+
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (SkipAuthorization(actionContext))
+            {
+                return;
+            }
+
+            var userSessionManager = new UserSessionManager(new TechShopData(new TechShopDbContext()));
+            if (userSessionManager.ValidateUserSession())
+            {
+                base.OnAuthorization(actionContext);
+            }
+            else
+            {
+                actionContext.Response = actionContext.ControllerContext.Request.CreateErrorResponse(
+                    HttpStatusCode.Unauthorized, "Access token expried or not valid.");
+            }
+        }
+
+        private static bool SkipAuthorization(HttpActionContext actionContext)
+        {
+            return actionContext.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().Any()
+                   || actionContext.ControllerContext.ControllerDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().Any();
+        }
+    }
+}
